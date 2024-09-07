77444b45ac27b1574a39283a8f8cd7d52bf8e16d
Leonardo Sales de Carvalho Jr <leoemcasa@hotmail.com>
2024-09-07 07:12:50 -0300

    RNCourse ANavigation pass param btween screens

--- RNCourseANavigation/screens/MealsOverviewScreen.js
+++ RNCourseANavigation/screens/MealsOverviewScreen.js
@@ -1,12 +1,25 @@
-import { MEALS } from "../data/dummy-data";
 import { FlatList, StyleSheet, Text, View } from "react-native";
+import MealItem from "../components/MealItem";
+import { MEALS } from "../data/dummy-data";
 
 function MealsOverviewScreen({ route }) {
     const catId = route.params.categoryId;
 
+    const displayedMeals = MEALS.filter((mealItem) => {
+        return mealItem.categoryIds.indexOf(catId) >= 0
+    });
+
+    function renderMealItem(itemData) {
+        return <MealItem title={itemData.item.title} />;
+    }
+
     return (
         <View style={styles.container}>
-            <Text style={styles.title}>Meals Overview {catId} </Text>
+            <FlatList
+                data={displayedMeals}
+                keyExtractor={(item) => item.id}
+                renderItem={renderMealItem}
+            />
         </View>
     );
 }
