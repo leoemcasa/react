f886d3a519f19ab9412c61b22cd41c0d2d6f2f91
Leonardo Sales de Carvalho Jr <Leonardo C Jr>
2024-09-21 11:20:08 -0300

    RNCourse ANavigation add button to head

--- RNCourseANavigation/screens/MealDetailScreen.js
+++ RNCourseANavigation/screens/MealDetailScreen.js
@@ -1,15 +1,36 @@
+import { useLayoutEffect } from 'react';
 import { View, Text, Image, StyleSheet, ScrollView } from 'react-native';
 
+import IconButton from '../components/IconButton';
 import List from '../components/MealDetail/List';
 import SubTitle from '../components/MealDetail/Subtitle';
 import { MEALS } from '../data/dummy-data';
 import MealDetails3 from '../components/MealDetails3';
 
-function MealDetailScreen({ route }) {
+function MealDetailScreen({ route, navigation }) {
 
     const mealId = route.params.mealId
 
     const selectedMeal = MEALS.find((meal) => meal.id === mealId);
+
+    function headerButtonPressHandler() {
+        console.log('Button pressed!');
+    }
+
+    useLayoutEffect(() => {
+        navigation.setOptions({
+            headerRight: () => {
+                // return <Button title='Tap me' onPress={headerButtonPressHandler}/>
+                return (
+                    <IconButton
+                        onPress={headerButtonPressHandler} 
+                        icon='star'
+                        color='white'
+                    />
+                )
+            }
+        });
+    }, [navigation, headerButtonPressHandler]);
 
     return (
         <ScrollView style={styles.rootContainer}>
